02b0f0ff6d2a1bc27bf2b0bb38084e3971ed6c00
mason125 <mason.white@snhu.edu>
2017-12-31 22:59:38 -0600

    F to C and Me!
    Completed app works.  Needs the ads plugin

--- TempConverter/F To C and Me/platforms/browser/www/Converter.js
+++ TempConverter/F To C and Me/platforms/browser/www/Converter.js
@@ -1,1 +1,68 @@
-document.getElementById("k").innerHTML = "test23";
+"use strict";
+//input f 
+const f = function(input)
+{
+	let c = ((input - 32) * (5/9));
+	let k = (c + 273.15);
+	return[c,k];
+}; 
+
+//input c
+const c = function(input)
+{
+	let f = ((input * (9/5)) + 32);
+	let k = (f + 459.67) *(5/9);
+	return [f,k];
+};
+
+//input k 
+const k = function(input)
+{
+	let f = ((input - 273.15)*(9/5) + 32)
+	let c = (input - 273.15)
+	return[f,c];
+};
+
+//convertion selection 
+function main(){
+	//temp vars
+	let converted_temp = [];//hold the converted tempatures
+	let selection = document.getElementById("unit").value;
+	let input = document.getElementById("val").value
+	
+	
+	//selection = 1;
+	switch(selection)
+	{
+		case "1"://input f
+			converted_temp = f(input);
+			document.getElementById("c").innerHTML = converted_temp[0].toFixed(2);
+			document.getElementById("k").innerHTML = converted_temp[1].toFixed(2);
+			document.getElementById("f").innerHTML = input;
+		break;
+		
+		case "2"://input c
+			converted_temp = c(input)
+			document.getElementById("f").innerHTML = converted_temp[0].toFixed(2);
+			document.getElementById("k").innerHTML = converted_temp[1].toFixed(2);
+			document.getElementById("c").innerHTML = input;
+		break;
+		
+		case "3"://input k
+			converted_temp = k(input);
+			document.getElementById("f").innerHTML = converted_temp[0].toFixed(2);
+			document.getElementById("c").innerHTML = converted_temp[1].toFixed(2);
+			document.getElementById("k").innerHTML = input;
+		break;
+		
+		default:
+			alert("Please Select a Temperature Unit");
+		break;
+		
+	}
+	
+}
+
+
+
+
